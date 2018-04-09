daeccead8406669151a06edff3485ff9a8cc98af
georgeGitM <zhiz2@student.unimelb.edu.au>
2018-04-09 19:32:03 +1000

    chess

--- Chess.hs
+++ Chess.hs
@@ -1,4 +1,4 @@
-module Chess (Color(..),Piece(..),Chess(..)) where
+module Piece (Color(..),Kind(..),Piece(..)) where
 
 import Data.List
 
@@ -6,10 +6,36 @@ data Color = Black | White
     deriving (Eq, Ord, Bounded, Enum)
 color_chars = "BW"
 
-data Piece = King | Queen | Rock | Bishop | Knight | Pawn
+data Kind = King | Queen | Rock | Bishop | Knight | Pawn
     deriving (Eq, Ord, Bounded, Enum)
-piece_chars = "KQRBKP"
+kind_chars = "KQRBKP"
 
-data Chess = Chess Color Piece
+data Piece = Piece Color Kind
     deriving (Eq, Bounded)
 
+instance Show Color where
+    show c = [color_chars !! fromEnum c]
+
+instance Show Kind where
+    show p = [kind_chars !! fromEnum p]
+
+instance Show Piece where
+    show (Piece c p) = show c ++ show p
+
+instance Read Color where
+    readsPrec _ = read_enum color_chars
+
+instance Read Kind where
+    readsPrec _ = read_enum kind_chars
+
+read_enum :: Enum e => String -> String -> [(e,String)]
+read_enum str (c:cs) =
+    case elemIndex c str of
+        Nothing -> []
+        Just i -> [(toEnum i, cs)]
+
+instance Read Piece where
+    readsPrec _ string = 
+        [(Piece c p,rest) | (p,rest0) <- reads string,
+                            (c,rest)  <- reads rest0]
+        
